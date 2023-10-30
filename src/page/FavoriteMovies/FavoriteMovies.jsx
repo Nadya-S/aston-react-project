335b323b6d5462bed0cf4cb901ae1023a98d3ffa
Atabask <aleksanin1396@mail.ru>
2023-10-30 19:08:25 +0300

    fix

--- src/page/FavoriteMovies/FavoriteMovies.jsx
+++ src/page/FavoriteMovies/FavoriteMovies.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import CardList from "../../components/CardList/CardList";
 import UpdateFavorites from "../../supabase/UpdateFavorites";
 import { useDispatch, useSelector } from "react-redux";
